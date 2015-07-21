1ebb7d03ef19cafaa9b48c0a6ef2b3c3381bf172
cypee <cypee@mmu.edu.my>
2015-07-21 14:46:15 +0800

    implementation of cache inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invmat <- NULL
+    set <- function(y) {
+        x <<- y
+        invmat <<- NULL
+    }
+    get <- function() x
+    setInvMatrix <- function(invmatrix) invmat <<- invmatrix
+    getInvMatrix <- function() invmat
+    list(set = set, get = get,
+         setInvMatrix = setInvMatrix,
+         getInvMatrix = getInvMatrix)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invm <- x$getInvMatrix()
+    if(!is.null(invm)) {
+        message("getting cached data")
+        return(invm)
+    }
+    data <- x$get()
+    rc = dim(data)
+    if(rc[1] != rc[2]) {
+        message("Not an nxn matrix. The matrix is not invertible")
+        return(NULL)
+    }
+    invm <- solve(data)
+    x$setInvMatrix(invm)
+    invm
 }
